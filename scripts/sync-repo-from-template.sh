323f7d45bae85b28a643a9f867543389d92f1822
Dan Serban <dan.serban@ejobsgroup.ro>
2022-04-10 13:18:26 +0300

    update sync script

--- scripts/sync-repo-from-template.sh
+++ scripts/sync-repo-from-template.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+REPOURL="git@github.com:Bibi40k/template-cluster-k3s.git"
 TEMPLATEURL="https://github.com/k8s-at-home/template-cluster-k3s"
 
-git remote add --track main template $TEMPLATEURL
-git remote set-url template $TEMPLATEURL
-git remote set-url --push template no_push
-git remote -v
+# Check if template remote was already added
+if ( ! git config --get remote.template.url | grep -q $TEMPLATEURL ); then
+    git remote add --track main template $TEMPLATEURL
+    git remote set-url template $TEMPLATEURL
+    git remote set-url --push template no_push
+    echo "Template remote added with no push access"
+    git remote -v
+else
+    echo "Template remote already exists"
+    git remote -v
+fi
+
 git pull template main # --allow-unrelated-histories
+
+echo "Push changes to main remote"
+git push
