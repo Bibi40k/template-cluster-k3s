0921357397b5da11e74fe168f7cebf3c2b7597a9
Dan Serban <dan.serban@ejobsgroup.ro>
2022-04-12 05:09:23 +0300

    Add podinfo Kustomization

--- runMe-Z-InstallFluxWithTestApp.sh
+++ runMe-Z-InstallFluxWithTestApp.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC2155
-export PROJECT_DIR=$(git rev-parse --show-toplevel)
+GITHUB_USER="Bibi40k"
+REPONAME="template-cluster-k3s"
+FLUXWATCHDIR="./cluster/test"
+
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=$REPONAME \
+  --branch=main \
+  --path=$FLUXWATCHDIR \
+  --personal
 
 flux create source git podinfo \
   --url=https://github.com/stefanprodan/podinfo \
   --branch=master \
   --interval=30s \
-  --export > ${PROJECT_DIR}/cluster/test/podinfo-source.yaml
+  --export > $FLUXWATCHDIR/podinfo-source.yaml
 
 git add -A && git commit -m "Add podinfo GitRepository"
 git push
@@ -18,7 +26,7 @@ flux create kustomization podinfo \
   --path="./kustomize" \
   --prune=true \
   --interval=5m \
-  --export > ${PROJECT_DIR}/cluster/test/podinfo-kustomization.yaml
+  --export > $FLUXWATCHDIR/podinfo-kustomization.yaml
 
 git add -A && git commit -m "Add podinfo Kustomization"
 git push
